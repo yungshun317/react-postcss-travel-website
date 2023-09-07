2eed61503a53c927f454887f129ce8b1701105de
yungshun317 <seraph317@gmail.com>
2023-09-07 19:44:15 +0800

    [FEAT] Apply mobile-first design using mixins

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 postCSSPlugins = [
     require("postcss-import"),
+    require("postcss-mixins"),
     require("postcss-simple-vars"),
     require("postcss-nested"),
     require("autoprefixer")
