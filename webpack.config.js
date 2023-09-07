91ebea404b450d3fea5c94d66f0fb97a98a2e648
yungshun317 <seraph317@gmail.com>
2023-09-07 19:36:04 +0800

    [CHORE] Use webpack with a development server that provides live reloading

--- webpack.config.js
+++ webpack.config.js
@@ -13,8 +13,17 @@ module.exports = {
         filename: 'bundled.js',
         path: path.resolve(__dirname, 'app')
     },
+    devServer: {
+      watchFiles: [ "app/**/*.html" ],
+      static: {
+          directory: path.join(__dirname, "app"),
+          watch: false
+      },
+      hot: true,
+      port: 3000
+    },
     mode: 'development',
-    watch: true,
+    // watch: true,
     module: {
         rules: [
             {
