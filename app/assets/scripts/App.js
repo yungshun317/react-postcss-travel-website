519d14145d351fa7bce157385f8c61e5ee72de9a
yungshun317 <seraph317@gmail.com>
2023-09-10 02:29:26 +0800

    [FEAT] Use object-oriented programming in `App.js`

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,3 +3,39 @@ import '../styles/styles.css';
 if (module.hot) {
     module.hot.accept();
 }
+
+/*
+var john = {
+    name: "John Doe",
+    favoriteColor: "blue",
+    greet: function() {
+        console.log("Hello, my name is " + john.name + " and my favorite color is " + john.favoriteColor + ".")
+    }
+}
+
+john.greet();
+
+var jane = {
+    name: "Jane Smith",
+    favoriteColor: "green",
+    greet: function() {
+        console.log("Hello, my name is " + jane.name + " and my favorite color is " + jane.favoriteColor + ".")
+    }
+}
+
+john.greet();
+ */
+
+function Person(fullName, favColor) {
+    this.name = fullName;
+    this.favoriteColor = favColor;
+    this.greet = function() {
+        console.log("Hello, my name is " + this.name + " and my favorite color is " + this.favoriteColor + ".");
+    }
+}
+
+let john = new Person("John Doe", "blue");
+john.greet();
+
+let jane = new Person("Jane Smith", "green");
+jane.greet();
--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ postCSSPlugins = [
     require("postcss-mixins"),
     require("postcss-simple-vars"),
     require("postcss-nested"),
+    require("postcss-hexrgba"),
     require("autoprefixer")
 ]
 
