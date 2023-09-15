28e304bc7d53545dd7c0845ebd08fb7905f6a9c4
yungshun317 <seraph317@gmail.com>
2023-09-16 04:30:15 +0800

    [FEAT] Throttle scroll events with Lodash

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -1,20 +1,26 @@
+import throttle from 'lodash/throttle';
+
 class RevealOnScroll {
     constructor() {
         this.itemsToReveal = document.querySelectorAll(".feature-item");
         this.hideInitially();
+        this.scrollThrottle = throttle(this.calcCaller, 200).bind(this);
         this.events();
     }
 
     events() {
-        window.addEventListener("scroll", () => {
-            this.itemsToReveal.forEach(el => {
-                this.calculateIfScrolledTo(el);
-            })
+        window.addEventListener("scroll", this.scrollThrottle);
+    }
+
+    calcCaller() {
+        console.log("Scroll function ran.");
+        this.itemsToReveal.forEach(el => {
+            this.calculateIfScrolledTo(el);
         })
     }
 
     calculateIfScrolledTo(el) {
-        console.log(el.getBoundingClientRect().top);
+        // console.log(el.getBoundingClientRect().top);
         let scrollPercent = (el.getBoundingClientRect().top / window.innerHeight) * 100;
         if (scrollPercent < 75) {
             el.classList.add("reveal-item--is-visible");
