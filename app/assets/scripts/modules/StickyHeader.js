c441be8953217a8f7fa1c9c14d20b4508d34dd99
yungshun317 <seraph317@gmail.com>
2023-10-19 20:42:24 +0800

    [FEAT] Detect scroll direction

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -6,6 +6,7 @@ class StickyHeader {
         this.siteHeader = document.querySelector(".site-header");
         this.pageSections = document.querySelectorAll(".page-section");
         this.browserHeight = window.innerHeight;
+        this.previousScrollY = window.scrollY;
         this.events();
     }
 
@@ -17,6 +18,8 @@ class StickyHeader {
     }
 
     runOnScroll() {
+        this.determineScrollDirection();
+
         if (window.scrollY > 60) {
             this.siteHeader.classList.add("site-header--dark");
         } else {
@@ -26,10 +29,22 @@ class StickyHeader {
         this.pageSections.forEach(el => this.calcSection(el));
     }
 
+    determineScrollDirection() {
+        if (window.scrollY > this.previousScrollY) {
+            this.scrollDirection = 'down';
+        } else {
+            this.scrollDirection = 'up';
+        }
+        this.previousScrollY = window.scrollY;
+    }
+
     calcSection(el) {
         if (window.scrollY + this.browserHeight > el.offsetTop && window.scrollY < el.offsetTop + el.offsetHeight) {
             let scrollPercent = el.getBoundingClientRect().top / this.browserHeight * 100;
-            if (scrollPercent < 18 && scrollPercent > -0.1) {
+            if (
+                scrollPercent < 18 && scrollPercent > -0.1 && this.scrollDirection == 'down' ||
+                scrollPercent < 33 && this.scrollDirection == 'up'
+            ) {
                 let matchingLink = el.getAttribute("data-matching-link");
                 document.querySelectorAll(`.primary-nav a:not(${matchingLink})`).forEach(el => el.classList.remove("is-current-link"));
                 document.querySelector(matchingLink).classList.add("is-current-link");
