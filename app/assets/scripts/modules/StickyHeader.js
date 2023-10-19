2c612654122a005e98ba4cd9d622446c60ac19d6
yungshun317 <seraph317@gmail.com>
2023-10-19 20:27:07 +0800

    [FEAT] Highlight current page section

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -1,13 +1,19 @@
 import throttle from 'lodash/throttle';
+import debounce from "lodash/debounce";
 
 class StickyHeader {
     constructor() {
         this.siteHeader = document.querySelector(".site-header");
+        this.pageSections = document.querySelectorAll(".page-section");
+        this.browserHeight = window.innerHeight;
         this.events();
     }
 
     events() {
         window.addEventListener("scroll", throttle(() => this.runOnScroll(), 200));
+        window.addEventListener("resize", debounce(() => {
+            this.browserHeight = window.innerHeight;
+        }, 333));
     }
 
     runOnScroll() {
@@ -15,6 +21,19 @@ class StickyHeader {
             this.siteHeader.classList.add("site-header--dark");
         } else {
             this.siteHeader.classList.remove("site-header--dark");
+        }
+
+        this.pageSections.forEach(el => this.calcSection(el));
+    }
+
+    calcSection(el) {
+        if (window.scrollY + this.browserHeight > el.offsetTop && window.scrollY < el.offsetTop + el.offsetHeight) {
+            let scrollPercent = el.getBoundingClientRect().top / this.browserHeight * 100;
+            if (scrollPercent < 18 && scrollPercent > -0.1) {
+                let matchingLink = el.getAttribute("data-matching-link");
+                document.querySelectorAll(`.primary-nav a:not(${matchingLink})`).forEach(el => el.classList.remove("is-current-link"));
+                document.querySelector(matchingLink).classList.add("is-current-link");
+            }
         }
     }
 }
