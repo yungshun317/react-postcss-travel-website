3aea90b3e374fa9d15865a8b72142a7cfb15b0e8
yungshun317 <seraph317@gmail.com>
2023-10-20 21:13:19 +0800

    [FEAT] Inject HTML from the `Modal` JavaScript module

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,11 +3,13 @@ import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 let mobileMenu = new MobileMenu();
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 60);
 let stickyHeader = new StickyHeader();
+new Modal();
 
 if (module.hot) {
     module.hot.accept();
