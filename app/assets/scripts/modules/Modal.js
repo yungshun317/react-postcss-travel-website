87f194b520b0ed5dc6685ecbd916fdcb53447a6c
yungshun317 <seraph317@gmail.com>
2023-10-20 21:19:21 +0800

    [FEAT] Handle the events that open and close the modal

--- app/assets/scripts/modules/Modal.js
+++ app/assets/scripts/modules/Modal.js
@@ -1,6 +1,36 @@
 class Modal {
     constructor() {
         this.injectHTML();
+        this.modal = document.querySelector(".modal");
+        this.closeIcon = document.querySelector(".modal__close");
+        this.openModalButtons = document.querySelectorAll(".open-modal");
+        this.events();
+    }
+
+    events() {
+        // Listen for open click
+        this.openModalButtons.forEach(el => el.addEventListener("click", e => this.openTheModal(e)));
+
+        // Listen for close click
+        this.closeIcon.addEventListener("click", () => this.closeTheModal());
+
+        // Push any key
+        document.addEventListener("keyup", e => this.keyPressHandler(e));
+    }
+
+    keyPressHandler(e) {
+        if (e.keyCode == 27) {
+            this.closeTheModal();
+        }
+    }
+
+    openTheModal(e) {
+        e.preventDefault();
+        this.modal.classList.add("modal--is-visible");
+    }
+
+    closeTheModal() {
+        this.modal.classList.remove("modal--is-visible");
     }
 
     injectHTML() {
